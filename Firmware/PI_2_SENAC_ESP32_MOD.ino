9058622c4cb60af6bf318e9ee9efe1826c24b8c1
Ewerson <ewerson@msn.com>
2017-11-11 18:57:27 -0200

    Add files via upload

---
+++ Firmware/PI_2_SENAC_ESP32_MOD.ino
@@ -0,0 +1,127 @@
+#if CONFIG_FREERTOS_UNICORE
+#define ARDUINO_RUNNING_CORE 0
+#else
+#define ARDUINO_RUNNING_CORE 1
+#endif
+
+#include "sensor_ultrason.h"
+#include "sensor_fluxo.h"
+#include "wifi_connect.h"
+#include "envia_recebe_dados.h"
+#include "data_hora.h"
+#include "display.h"
+
+// Funções Multitarefas
+void tarefa1( void * pvParameters ) {
+  while (true) {
+    conta_litros();
+    mede_distacia();
+    RtcDateTime now = Rtc.GetDateTime();
+    printDateTime(now);
+    Display();
+    Serial.print("   Core: ");
+    Serial.print(xPortGetCoreID());
+    Serial.print("   ");
+    delay(1000);
+  }
+}
+
+void tarefa2( void * pvParameters ) {
+  while (true) {
+    connectToNetwork();
+    conectaDB();
+    Serial.print("   Core: ");
+    Serial.print(xPortGetCoreID());
+    Serial.print("   ");
+    delay(10000);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  // Sensor Utrasônico
+  pinMode(trigPin, OUTPUT); // Define o trigPin como uma saída
+  pinMode(echoPin, INPUT); // Define o echoPin como uma Entrada
+
+  // Sensor de Fluxo
+  // Inicializacao da variavel "flowmeter" como INPUT (pino 15)
+  pinMode(flowmeter, INPUT);
+  // Anexe uma interrupção ao vetor flowmeter
+  attachInterrupt(digitalPinToInterrupt(flowmeter), flow, RISING); // Configuração de interrupção
+  // veja http://arduino.cc/en/Reference/attachInterrupt
+
+  // Inicia Display TFT
+  tft.init();
+  tft.setRotation(1);
+
+  // Imprimo no display informações da inicializacão
+  tft.setTextSize(1);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.drawString("ENGENHARIA", 10, 5, 1);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.drawString("de", 45, 15, 1);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.drawString("COMPUTACAO", 60, 15, 1);
+  tft.setTextColor(TFT_GREEN, TFT_BLACK);
+  tft.drawString("PI2 SENAC", 3, 30, 4);
+  tft.setTextColor(TFT_YELLOW, TFT_BLACK);
+  tft.drawString("Iniciando...", 30, 60, 2);
+
+  // Inicia RTC
+  Rtc.Begin();
+  Rtc.Enable32kHzPin(false);
+  Rtc.SetSquareWavePin(DS3231SquareWavePin_ModeNone);
+
+
+  // Inicia SDCard
+  tft.setTextColor(TFT_BLUE, TFT_BLACK);
+  tft.drawString("Lendo SDCard: ", 10, 105, 1);
+  if (!SD.begin()) {
+    Serial.println("Falha na montagem do cartão");
+
+    tft.setTextColor(TFT_RED, TFT_BLACK);
+    tft.drawString("Falha!", 90, 105, 1);
+    delay ( 2000 );
+    tft.fillScreen(TFT_BLACK);
+    return;
+  }
+  uint8_t cardType = SD.cardType();
+
+  if (cardType == CARD_NONE) {
+    Serial.println("Nenhum cartão SD anexado");
+    return;
+  }
+
+  Serial.print("SD Card Type: ");
+  if (cardType == CARD_MMC) {
+    Serial.println("MMC");
+  } else if (cardType == CARD_SD) {
+    Serial.println("SDSC");
+  } else if (cardType == CARD_SDHC) {
+    Serial.println("SDHC");
+    tft.setTextColor(TFT_GREEN, TFT_BLACK);
+    tft.drawString("OK!", 90, 105, 1);
+  } else {
+    Serial.println("DESCONHECIDO");
+  }
+
+  uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+  Serial.printf("Tamanho do SDCard: %lluMB\n", cardSize);
+  Serial.printf("Espaço total: %lluMB\n", SD.totalBytes() / (1024 * 1024));
+  Serial.printf("Espaço usado: %lluMB\n", SD.usedBytes() / (1024 * 1024));
+  delay ( 2000 );
+  tft.fillScreen(TFT_BLACK);
+  // Inicia multitarefas
+
+  xTaskCreatePinnedToCore(tarefa1, "loop1", 4096, NULL, 1, NULL, ARDUINO_RUNNING_CORE);
+  xTaskCreatePinnedToCore(tarefa2, "loop2", 4096, NULL, 1, NULL, ARDUINO_RUNNING_CORE);
+
+}
+
+void loop() {
+  // Não faça nada aqui
+  vTaskDelay(portMAX_DELAY); // aguarde o máximo possível ...
+
+}
---
+++ Firmware/sensor_fluxo.h
@@ -0,0 +1,49 @@
+#ifndef sensor_fluxo_h
+#define sensor_fluxo_h
+#include "config.h"
+
+// Sensor de Fluxo
+volatile int  flow_frequency;  // Mede os pulsos do sensor de fluxo
+unsigned int  l_hour;          // Calcula litro/hora
+int litrosAdd = 0;
+float litros = 0;
+//unsigned char flowmeter = 15;  // Número do pino do sensor de fluxo
+int contagem_comp = 0;
+
+// Função de interrupção, de modo que a contagem do pulso "aumenta" não interfira com o resto do código (attachInterrupt)
+void flow ()                  // Função de interrupção
+{
+  flow_frequency++;
+  // Se a contagem de transições (de baixo para alto, "subida") é superior a 440, conte com mais um litro.
+  // pulso por litro +/- 450 "www.hobbytronics.co.uk/yf-s201-water-flow-meter"
+  if (flow_frequency > 440 )
+  {
+    litrosAdd++;
+    flow_frequency = 0;
+  }
+  litros = litrosAdd + flow_frequency / 450.0;
+}
+
+float sens_fluxo = 0;
+
+void conta_litros() {
+  //Sensor Fluxo
+  if (contagem_comp != flow_frequency) {
+
+    // Pulse frequency (Hz) = 7.5Q, Q é taxa de fluxo em L/min. (Resultados em +/- 3%)
+    l_hour = (flow_frequency * 60 / 7.5); // (Freqüência de pulso x 60 min) / 7.5Q = taxa de fluxo em L/hour
+    Serial.print("Fluxo em litros: ");
+    Serial.print (l_hour);
+
+    Serial.print("   ");
+    Serial.print(l_hour, DEC);            // Imprime L/hour
+    Serial.print(" L/hour     ");
+
+    Serial.print(litros);            // Imprime litros/hour
+    Serial.println(" Litros");
+    contagem_comp = flow_frequency;
+  }
+  sens_fluxo = litros;
+}
+#endif
+
